b5996498e8fadec97a7a2664e0f5ae71744d508d
loader3229 <1010903229@qq.com>
2026-02-03 14:21:10 +0806

    commit

--- test.py
+++ test.py
@@ -4,6 +4,10 @@ import pyzbar.pyzbar as pyzbar
 import random
 import os
 import sys
+import io
+
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+sys.stderr = io.TextIOWrapper(sys.stderr.buffer, encoding='utf-8')
 
 
 
